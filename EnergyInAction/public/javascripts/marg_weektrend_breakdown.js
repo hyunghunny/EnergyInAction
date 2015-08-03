674f7fa28d855be0c83dc1ddf262d02aef80ede1
SuperBobby <hanjy@snu.ac.kr>
2015-08-03 23:21:17 +0900

    Day trend 완성
    추후 오늘 날짜 기준으로 조회하도록 수정할 필요 있음

--- EnergyInAction/public/javascripts/marg_weektrend_breakdown.js
+++ EnergyInAction/public/javascripts/marg_weektrend_breakdown.js
@@ -1,98 +1,126 @@
-var today = new Date();
-var dayTime = today.getTime();
+var dayLabel = new Array('일', '월', '화', '수', '목', '금', '토');
 
-var start_day = new Date(today);
-start_day.setDate(today.getDate() - 30);
+var baseDay     = new Date('2015-07-16');
+//var baseDay     = new Date(); // today 날짜 잡도록 시간 초기화가 필요함
+var lastWeekDay = new Date(baseDay-(3600000*24*7))
+//var toDay   = new Date('2015-04-04');
 
-var start_year  = start_day.getFullYear();
-var start_month = start_day.getMonth()+1;
-var start_date  = start_day.getDate();
-
-var from_date = start_day.setHours(9,0,0,0);
-
+// var baseTime     = baseDay.getTime() //  + 3600000*9; // for time shift
+// var lastWeekTime = lastWeekDay.getTime();
+// var   toTime = toDay.getTime() / 1000;
 
 $(function () {
     //document.getElementById("date").innerHTML = (today.getMonth() + 1) + '월 ' + today.getDate() + '일 사용량';
-    console.log(start_day);
-    console.log(from_date);
-    console.log(start_year);
-    console.log(start_month);
-    console.log(start_date);
 
-    data_hvac = [5, 3, 4, 7, 2, 3, 4];
-    data_com  = [3, 4, 4, 2, 5, 6, 3];
-    data_light = [2, 5, 6, 2, 1, 3, 2];
-    data_etc = [3, 0.5, 4, 4, 3, 2, 1];
+    var xAxis_categories = new Array();
 
-    data_total = [13, 12.5, 18, 15, 11, 14, 10];
+    var data_total = [];
+    var data_hvac  = []; //5, 3, 4, 7, 2, 3, 4];
+    var data_com   = []; //3, 4, 4, 2, 5, 6, 3];
+    var data_light = []; //2, 5, 6, 2, 1, 3, 2];
+    var data_etc   = []; //3, 0.5, 4, 4, 3, 2, 1];
 
-    //showChart();
-    $('#container').highcharts({
-            chart: {
-                type: 'column'
-            },
+    query = 'api/labs/marg/energy/daily.json?day_from=' + dateFormatter(lastWeekDay) + '&day_to=' + dateFormatter(baseDay) + '&offset=9';
+    invokeOpenAPI(query, function (data) {
+      console.log(data);
 
-            title: {
-                text: '지난 일주일 전력 사용 변화'
-            },
+      for(var index = 0; index < data.length; index++){
+        total = data[index].sum;
+        hvac = accumulater(data[index], 'hvac');
+        com  = accumulater(data[index], 'computer');
+        light = accumulater(data[index], 'light');
+        etc = total - (hvac + com + light);
 
-            xAxis: {
-                categories: ['7/20(월)', '7/21(화)', '7/22(수)', '7/23(목)', '7/24(금)', '어제', '오늘 (14시 기준)']
-            },
+        date_label = data[index].dateFrom.substring(0,10);
+        day_label = new Date(date_label);
+        day_label = dayLabel[day_label.getDay()];
+        xAxis_categories.push(date_label + '(' + day_label+ ')');
 
-            yAxis: {
-                allowDecimals: false,
-                min: 0,
-                title: {
-                    text: '전력 사용량 (kW/h)'
-                }
-            },
+        data_total.push(Number(total.toFixed(1)));
+        data_hvac.push(hvac);
+        data_com.push(com);
+        data_light.push(light);
+        data_etc.push(etc);
+      }
 
-            tooltip: {
-                formatter: function () {
-                    return '<b>' + this.x + '</b><br/>' +
-                        this.series.name + ': ' + this.y + '<br/>' +
-                        'Total: ' + this.point.stackTotal;
-                }
-            },
+      console.log('total', data_total);
+      console.log('hvac', data_hvac);
+      console.log('com', data_com);
+      console.log('light', data_light);
+      console.log('etc', data_etc);
 
-            plotOptions: {
-                column: {
-                    stacking: 'normal'
-                }
-            },
+      //showChart();
+      $('#container').highcharts({
+              chart: {
+                  type: 'column'
+              },
 
-            series: [{
-                name: '냉난방',
-                data: data_hvac,
-                stack: 'stack'
-            }, {
-                name: '컴퓨터',
-                data: data_com,
-                stack: 'stack'
-            }, {
-                name: '전등',
-                data: data_light,
-                stack: 'stack'
-            }, {
-                name: '기타',
-                data: data_etc,
-                stack: 'stack'
-            }, {
-                type: 'spline',
-                name: '전체',
-                data: data_total,
-                dataLabels: {
-                    enabled: true,
-                    format: '{y}'
-                },
-                marker: {
-                    lineWidth: 2,
-                    lineColor: Highcharts.getOptions().colors[0],
-                    fillColor: 'white'
-                }
-            }]
-        });
+              title: {
+                  text: '지난 일주일 전력 사용 변화'
+              },
+
+              xAxis: {
+                  categories: xAxis_categories
+              },
+
+              yAxis: {
+                  allowDecimals: false,
+                  min: 0,
+                  title: {
+                      text: '전력 사용량 (kW/h)'
+                  }
+              },
+
+              tooltip: {
+                  formatter: function () {
+                      return '<b>' + this.x + '</b><br/>' +
+                          this.series.name + ': ' + this.y + '<br/>' +
+                          'Total: ' + this.point.stackTotal;
+                  }
+              },
+
+              plotOptions: {
+                  column: {
+                      stacking: 'normal'
+                  }
+              },
+
+              series: [{
+                  name: '냉난방',
+                  data: data_hvac,
+                  stack: 'stack',
+                  color: '#8a2be2'
+              }, {
+                  name: '컴퓨터',
+                  data: data_com,
+                  stack: 'stack',
+                  color: '#70AD47'
+              }, {
+                  name: '전등',
+                  data: data_light,
+                  stack: 'stack',
+                  color: '#ffd700'
+              }, {
+                  name: '기타',
+                  data: data_etc,
+                  stack: 'stack',
+                  color: '#808080'
+              }, {
+                  type: 'spline',
+                  name: '전체',
+                  data: data_total,
+                  dataLabels: {
+                      enabled: true,
+                      format: '{y}'
+                  },
+                  marker: {
+                      lineWidth: 2,
+                      lineColor: '#1e90ff',
+                      fillColor: 'white'
+                  }
+              }]
+          });
+      });
 });
 
 
@@ -113,4 +141,25 @@ function invokeOpenAPI(url, scb) {
 
         }
     });
+}
+
+function accumulater(data, targetDescription) {
+  result = 0;
+  for(i=0; i<data.feeders.length;i++){
+      if(data.feeders[i].description == targetDescription) {
+        result = result + (data.feeders[i].value);
+        console.log(data.feeders[i].value, data.feeders[i].description, result)
+      }
+  }
+  return result;
+}
+
+function dateFormatter(input_date){
+  //console.log('format year', input_date.getFullYear());
+  //console.log('format month', input_date.getMonth()+1);
+  //console.log('format date', input_date.getDate());
+
+  result = input_date.getFullYear() + '-' + (input_date.getMonth()+1) + '-' + input_date.getDate();
+  //console.log(result);
+  return result;
 }
