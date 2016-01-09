0153271f0f12c604331053e3c299605fe5c9f190
SuperBobby <hanjy@snu.ac.kr>
2016-01-09 15:18:36 +0900

    마그 리얼타임 맥스 조정
    10에서 7로

--- EnergyInAction/public/javascripts/marg/marg_meter_v2.js
+++ EnergyInAction/public/javascripts/marg/marg_meter_v2.js
@@ -49,7 +49,7 @@ $(function () {
         // the value axis
         yAxis: {
             min: 0,
-            max: 10.0,
+            max: 7,
             stops: [
                 [0.1, '#197F00'], // green
                 [0.8, '#DDDF0D'], // yellow
